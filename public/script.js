03fbb299de73ab4178e9d416d741ef4e832fbe49
Jose Mateus <jolmateu@byui.edu>
2023-10-14 22:34:07 -0500

    MongoDB Added

--- public/script.js
+++ public/script.js
@@ -1,39 +1,65 @@
-// Gets the task list saved in localStorage or creates an empty list if it does not exist
-let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+// Gets the task list saved in MongoDB
+document.addEventListener('DOMContentLoaded', function() {
+  // Fetch tasks from the server and display them
+  fetch('/tasks')
+    .then(response => response.json())
+    .then(tasks => {
+      viewTasks(tasks);
+    })
+    .catch(error => console.error(error));
 
-function viewTasks(){
-    // Select the elemnts from the task list
-    const taskList = document.querySelector('.list');
-    taskList.innerHTML = '';
+  function viewTasks(tasks){
+      // Select the elemnts from the task list
+      const taskList = document.querySelector('.list');
+      taskList.innerHTML = '';
 
-    // Add event listeners to the tasks buttons
-    tasks.forEach(task => {
-        const taskElement = document.createElement('li');
-        taskElement.className = task.completed ? 'verified' : '';
-        taskElement.innerHTML = `<i class="check">&#x2714;</i> ${task.text} <i class="remove">&#x2717;</i>`;
-        taskList.appendChild(taskElement);
+      // Add event listeners to the tasks buttons
+      tasks.forEach(task => {
+          const taskElement = document.createElement('li');
+          taskElement.className = task.completed ? 'verified' : '';
+          taskElement.innerHTML = `<i class="check">&#x2714;</i> ${task.text} <i class="remove">&#x2717;</i>`;
+          taskList.appendChild(taskElement);
 
-        const checkButton = taskElement.querySelector('.check');
-        const removeButton = taskElement.querySelector('.remove');
+          const checkButton = taskElement.querySelector('.check');
+          const removeButton = taskElement.querySelector('.remove');
 
-        checkButton.addEventListener('click', function() {
-            task.completed = !task.completed;
-            viewTasks();
-            updateTasks();
-        });
+          checkButton.addEventListener('click', function() {
+              task.completed = !task.completed;
+              updateTask(task);
+          });
 
-        removeButton.addEventListener('click', function() {
-            tasks = tasks.filter(t => t !== task);
-            viewTasks();
-            updateTasks();
-        });
-    });
+          removeButton.addEventListener('click', function() {
+              removeTask(task);
+          });
+      });
+  }
+
+function updateTask(task) {
+  fetch(`/tasks/${task._id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+  .then(response => response.json())
+  .then(updatedTask => {
+    viewTasks(updatedTask);
+  })
+  .catch(error => console.error(error));
+  console.log("Error en PUT");
+  console.log(updatedTask);
 }
 
-function updateTasks() {
-    let string = JSON.stringify(tasks);
-    localStorage.setItem('tasks', string);
-    /* console.log(tasks); */
+function removeTask(task) {
+  fetch(`/tasks/${task._id}`, {
+    method: 'DELETE'
+  })
+  .then(response => response.json())
+  .then(updatedTasks => {
+    viewTasks(updatedTasks);
+  })
+  .catch(error => console.error(error));
 }
 
 // Event listener to the form to add new task
@@ -44,17 +70,26 @@ form.addEventListener('submit', function(event) {
     const newTaskText = document.getElementById('ntask').value;
     if (newTaskText) {
         const newTask = {
-            text: newTaskText,
-            completed: false
+          _id: Math.floor(Math.random() * 1000000).toString(),
+          text: newTaskText,
+          completed: false
         };
 
-        tasks.push(newTask);
-        viewTasks();
-        updateTasks();
+        fetch('/tasks', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(newTask)
+        })
+        .then(response => response.json())
+        .then(updatedTasks => {
+          viewTasks(updatedTasks);
+        })
+        .catch(error => console.error(error));
 
         // Clean the text input when the task is added
         document.getElementById('ntask').value = '';
     }
+  });
 });
-
-viewTasks();
--- server.js
+++ server.js
@@ -1,14 +1,53 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+const uri = "mongodb+srv://jolmateu:8bO7WE2zMSTrCxSZ@cluster0.3mvfv.mongodb.net/?retryWrites=true&w=majority";
+
+const DB_NAME = 'todo';
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function startServer() {
+  try {
+    await client.connect();
+    console.log('Connected to the database');
+    const db = client.db(DB_NAME);
+    const tasksCollection = db.collection('tasks');
+
+    // Ruta para obtener las tareas desde la base de datos
+    app.get('/tasks', async (req, res) => {
+      const tasks = await tasksCollection.find({}).toArray();
+      res.json(tasks);
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // If there is an error, this will help prevent the Node.js application from hanging
+    // It's a best practice to close the client when you are done with it
+    console.log("Este es el finally");
+  }
+}
+
+startServer().catch(console.dir);
